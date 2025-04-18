69024dada1051a0ab790ae174c1fd75c0c647a65
finanzaignorante <finanzaignorante@gmail.com>
2025-04-18 17:55:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var yt = function(){
+    window.location = "https://www.youtube.com/@FinanzaIgnorantePodcast";
+}
+var ig = function(){
+    window.location = "https://www.instagram.com/finanzaignorante?igsh=aDlvdnY4aHlhdTNh";
+}
+
+var spotify = function(){
+    window.location = "https://open.spotify.com/show/6RwFR2fA987dphL8RY84SE#:~:text=Dovresti%20ascoltare%20Finanza%20Ignorante%20perch%C3%A9%20rende%20la%20gestione,risparmiare%2C%20investire%20e%20vivere%20con%20maggiore%20serenit%C3%A0%20finanziaria."
+}
+var applepodcast = function(){
+    window.location = "https://podcasts.apple.com/us/podcast/finanza-ignorante/id1787433740"
+}
